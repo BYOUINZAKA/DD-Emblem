e1999ca03b59af0e2ea5e5435e5f5ce74b465a81
BYOUINZAKA <2606675531@qq.com>
2020-03-29 10:50:24 +0800

    git commit -m "2020.03.29" git push

--- src/Base.py
+++ src/Base.py
@@ -5,31 +5,60 @@ import json
 
 class Navigator:
     def __init__(self, searchMsg):
+        """构造函数
+        接收一个代表检索方式信息的字典作为参数，并建立空对象。
+
+        Args:
+            searchMsg: 描述检索方式的信息，应为一个字典，可通过json报文转化所得，并满足以下结构：
+                {
+                    "keyword": "正在抽奖",               //检索关键词，通常为正在抽奖。
+                    "targets": [                        //检索目标的地址列表，一般为bilibili各直播大区。
+                        {
+                            "name": "...",
+                            "url: "https://.../..."
+                        },
+                        {
+                            "name": "...",
+                            "url: "https://.../.../..."
+                        }
+                        // ...
+                    ]
+                }
+
+        Raises:
+            并不会检索searchMsg的格式并抛出异常。
+        """
         self.LiveRoomList = []
-        self.Count = 0
         self.SearchMsg = searchMsg
 
     async def Loads(self, headers, basePage=1, topPage=3, count=-1):
-        baseCount = self.Count
+        baseCount = len(self.LiveRoomList)
         keyWord = self.SearchMsg.get('keyword')
         targets = self.SearchMsg.get('targets')
         for area in targets:
-            target = targets.get(area)
+            target = targets.get(area).get('url')
             for page in range(basePage, topPage+1):
                 response = {}
                 url = target % (page)
                 async with aiohttp.ClientSession() as session:
                     async with session.get(url, headers=headers) as res:
+                        # 请求成功则异步拉取json报文。
                         if res.status == 200:
                             response = json.loads(await res.text())
                         else:
                             continue
                 roomList = response.get('data').get('list')
+                # 如果显示正在抽奖的关键字，则根据直播间ID生成链接。
                 for roomData in roomList:
                     try:
                         if roomData.get('pendant_info').get('2').get('content') == keyWord:
-                            self.LiveRoomList.append(
-                                "https://live.bilibili.com/"+roomData.get('roomid'))
+                            self.LiveRoomList.append({
+                                'roomid': roomData.get('roomid'),
+                                'urid': roomData.get('uid'),
+                                'parent_id': roomData.get('parent_id'),
+                                'area_id': roomData.get('area_id'),
+                                'url': "https://live.bilibili.com/"+roomData.get('roomid')
+                            })
                             self.Count += 1
                             if self.Count-baseCount > count:
                                 return
--- src/main.py
+++ src/main.py
@@ -5,7 +5,8 @@ import Base
 
 
 async def main():
-    url = 'https://api.live.bilibili.com/room/v3/area/getRoomList?platform=web&parent_area_id=5&cate_id=0&area_id=0&sort_type=sort_type_8&page=1&page_size=30&tag_version=1'
+    # url = 'https://api.live.bilibili.com/room/v3/area/getRoomList?platform=web&parent_area_id=5&cate_id=0&area_id=0&sort_type=sort_type_8&page=1&page_size=30&tag_version=1'
+    url = "https://api.live.bilibili.com/room/v3/area/getRoomList?platform=web&parent_area_id=3&cate_id=0&area_id=0&sort_type=sort_type_121&page=1&page_size=30&tag_version=1"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as res:
             print(res.status)
@@ -13,11 +14,17 @@ async def main():
             # print(json.loads(await res.text()))
     roomList = response.get('data').get('list')
     for roomData in roomList:
+        # print(roomData['roomid'])
         try:
-            if roomData.get('pendant_info').get('2').get('content') == '正在抽奖':
+            if roomData.get('pendant_info').get('2').get('content') == "正在抽奖":
+                d = {
+                    'roomid': roomData.get('roomid'),
+                    'urid': roomData.get('uid'),
+                    'parent_id': roomData.get('parent_id'),
+                    'area_id': roomData.get('area_id'),
+                    'url': "https://live.bilibili.com/"+roomData.get('roomid')
+                }
                 print(roomData.get('roomid'))
-                if self.Count-baseCount > count:
-                    return
             else:
                 continue
         except:
@@ -26,22 +33,3 @@ async def main():
 loop = asyncio.get_event_loop()
 task = loop.create_task(main())
 loop.run_until_complete(task)
-d = {
-    '1': {
-        '11': 'abc',
-        '22': ''
-    },
-    '2': 'ab',
-    '3': {
-        '31': 'abc',
-        '32': {
-            '321': 'abcd'
-        }
-    }
-}
-for va in d:
-    try:
-        value = d[va]['11']
-    except:
-        continue
-    print(value)
