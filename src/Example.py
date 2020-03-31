0cac589a469b0a00fbcb505401b6d2d4d24c0d3a
BYOUINZAKA <2606675531@qq.com>
2020-03-31 14:38:10 +0800

    git push

--- src/Example.py
+++ src/Example.py
@@ -1,3 +1,4 @@
+# 示例代码
 import asyncio
 import json
 import time
@@ -17,11 +18,12 @@ def getMsgFromJsonFile(path: str) -> dict:
 if __name__ == '__main__':
     # 读取headers，需要包含Cookie，Content-Length，Referer字段。
     headers = getMsgFromJsonFile('E:\Python Tools\data\Headers.json')
+    # 构造Navigator对象需要传入一个字典作为搜索信息，一般使用SearchMsg.json中的内容来构筑。
     navi = Navigator(getMsgFromJsonFile('SearchMsg.json'))
 
     # 输入页数，Windows下打开文件限度为509，平均下来为80页。
     # Linux下为1000，平均160左右。
-    # 但是由于某些分区的直播数较少，所以设为100也不会有问题。
+    # 但是由于某些分区的直播数较少，所以Windows下设为100也不会有问题。
     topPage = int(input("输入最大页数："))
 
     start = time.time()
